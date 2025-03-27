34eef9d92570fac38e82ad9e3cd41ad55ae60860
Lian <lian.h.silva@aluno.senai.br>
2025-03-27 20:11:39 -0300

    Lian Henrique: update app export enviado com sucesso

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Cadastro from './Cadastro/Cadastro'
+import Login from './Login/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      
+      <Login />
+      <Cadastro />
     </div>
   )
 }
---
+++ src/Cadastro/Cadastro.jsx
@@ -0,0 +1,11 @@
+import styles from "./Cadastro.module.css";
+
+const Cadastro = () => {
+  return (
+    <div style={styles.body}>
+
+    </div>
+  )
+}
+
+export default Cadastro
