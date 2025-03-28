c4a35fccb2b0a28ed60281d50a05774a0e5b0c3f
Lian <lian.h.silva@aluno.senai.br>
2025-03-27 21:49:44 -0300

    Lian Henrique: update Cadastro

--- src/Components/Cadastro/Cadastro.jsx
+++ src/Components/Cadastro/Cadastro.jsx
@@ -2,8 +2,14 @@ import styles from "./Cadastro.module.css";
 
 const Cadastro = () => {
   return (
-    <div>
-      Cadastro
+    <div className={styles.body}>
+      <h1 className={styles.title}>Cadastro</h1>
+      <div className={styles.baseInput}>
+        <input className={styles.input} type="text" placeholder="Nome"/>
+        <input className={styles.input} type="text" placeholder="E-mail"/>
+        <input className={styles.input} type="password" placeholder="Senha"/>
+        <input className={styles.input} type="password" placeholder="Confirmação de Senha"/>
+      </div>
     </div>
   )
 }
