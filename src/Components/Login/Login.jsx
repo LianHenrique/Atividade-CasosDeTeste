7f4ed37eeb45ec97e949507914022957cb2192a0
Lian <lianhenrique1902@gmail.com>
2025-04-02 14:37:21 -0300

    Lian Henrique: update comentarios

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -4,8 +4,10 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
+  // Lista para coletar as informações do localStorage
   const [user, setUser] = useState([]);
 
+  // Coleta as indormações do localStorage
   useEffect(() => {
     const savedUsers = localStorage.getItem("usuarios");
     if (savedUsers) {
@@ -13,38 +15,48 @@ const Login = () => {
     }
   }, []);
 
+  // States que coletam as informações dos inputs
   const [emailInput, setEmailInput] = useState();
   const [senhaInput, setSenhaInput] = useState();
 
   const navegate = useNavigate();
 
-  // Função chamada no envio do formulário
-  const handleLogin = (e) => {
-    e.preventDefault();
-  };
-
+  // Função para confirmar login
   const logar = () => {
+    // Find para procurar se email e senha digitados existem
     const usuarioExiste = user.find(
       (u) => u.email === emailInput && u.senha === senhaInput
     );
     
+    // Confirmação do find
     if (!usuarioExiste) {
       alert("Login inválido");
     }
     else{
+      // Find returna o objeto encontrado com todos deus valores
       alert("Bem vindo "+usuarioExiste.nome);
     }
   };
 
   return (
-    <div className={styles.body}>
-      <h1 className={styles.title}>Login</h1>
-      <form className={styles.baseInput} onSubmit={handleLogin}>
+    <div 
+    className={styles.body}>
+      <h1 
+      className={styles.title}>
+        Login
+      </h1>
+      <form 
+      className={styles.baseInput} 
+      // para a tela não recarregar
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}>
         <div>
           <input
             className={styles.input} 
             type="email"
             id="email"
+            // Coleta o email
             onChange={(e) => setEmailInput(e.target.value)}
             placeholder="E-mail"
           />
@@ -55,6 +67,7 @@ const Login = () => {
             className={styles.input} 
             type="password"
             id="senha"
+            // Coleta a senha
             onChange={(e) => setSenhaInput(e.target.value)}
             placeholder="Senha"
           />
@@ -62,6 +75,7 @@ const Login = () => {
         <button 
         className={styles.button} 
         type="submit" 
+        // Navega até o cadastro
         onClick={() => {
           navegate("/cadastro")
         }}>
